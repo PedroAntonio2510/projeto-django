c2f9e44702bfc8d3cb30f294dc097f964762b601
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2024-07-29 12:12:30 -0300

    Arrumado funcao de registrar usuario

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(""),
+    path("", views.home, name='home'),  # Home page
+    path("register/", views.register_user, name='register_user'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import User
 
 # Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def register_user(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone_number = request.POST.get('phone_number')
+        
+        # Validação básica (adapte conforme necessário)
+    if not name or not email:
+            # Retornar uma mensagem de erro ou renderizar a página com um contexto de erro
+      return render(request, 'register_user.html', {'error': 'Todos os campos são obrigatórios'})
+    try:
+      user = User(name=name, email=email, phone_number=phone_number)
+      user.save()
+      return redirect(reverse('home'))  # Use o nome da view aqui
+    except Exception as e:
+      # Capturar o erro e renderizar a página com uma mensagem de erro
+      return render(request, 'register_user.html', {'error': str(e)})
+  return render(request, 'register_user.html')
