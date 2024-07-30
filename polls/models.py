b1a778cffba6ff7ecb135de56517f159d23a4032
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2024-07-30 13:52:09 -0300

    Removendo login, registro, peril de usuario

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-  name = models.CharField(max_length=100)
-  email = models.EmailField()
-  phone_number = models.CharField(max_length=20)
-  image = models.ImageField(upload_to='pics', default='default.svg')
-
-  def __str__(self):
-    return self.name
+class Product(models.Model):
+  pass
--- polls/views.py
+++ polls/views.py
@@ -1,51 +1,13 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.shortcuts import render, get_object_or_404
-from .models import User
+from django.shortcuts import render
+from django.shortcuts import render
+
 
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
 
-def register_user(request):
-  if request.method == 'POST':
-    name = request.POST.get('name')
-    email = request.POST.get('email')
-    phone_number = request.POST.get('phone_number')
-    image = request.FILES.get('image')
-    if not name or not email:
-      return render(request, 'register_user.html', {'error': 'Todos os campos são obrigatórios'})
-    try:
-      user = User(name=name, email=email, phone_number=phone_number, image=image)
-      user.save()
-      return redirect(reverse('home'))  # Use o nome da view aqui
-    except Exception as e:
-      # Capturar o erro e renderizar a página com uma mensagem de erro
-      return render(request, 'register_user.html', {'error': str(e)})
-  return render(request, 'register_user.html')
-
-def user_login(request):
-  if request.method == 'POST':
-    email = request.POST.get('email')
-    user = authenticate(request)
-    if user is not None:
-      login(request, user)
-      return redirect(reverse('home', id=id))
-    else:
-      messages.error(request, 'Email não encontrado.')
-  return render(request, 'login.html', {'message': 'Usuário ou senha inválidos'})
 
 
-@login_required
-def profile_user(request, id):
-    user = get_object_or_404(User, id=id)
-    return render(request, 'profile.html', {'user': user})
 
-def logout(request):
-  logout(request)
-  return redirect(reverse('home'))
+  
